1c99cb3a61fd62e52d146944353d802b6b3fa1d7
Airat Halitov <airat.halitov@gmail.com>
2020-12-18 10:36:08 +0500

    > /dev/null 2>&1

--- airat-redis-update.sh
+++ airat-redis-update.sh
@@ -9,11 +9,11 @@ NP=$(nproc)
 rm -rf redis-stable.tar.gz
 rm -rf redis-stable
 
-wget http://download.redis.io/redis-stable.tar.gz
+wget http://download.redis.io/redis-stable.tar.gz > /dev/null 2>&1
 
 if [ -f redis-stable.tar.gz ]; then
-    tar xvzf redis-stable.tar.gz && rm -rf redis-stable.tar.gz
+    tar xvzf redis-stable.tar.gz > /dev/null 2>&1 && rm -rf redis-stable.tar.gz
 
     cd redis-stable
-    make -j $NP && sudo make install && echo "Done!"
+    make -j $NP > /dev/null 2>&1 && sudo make install > /dev/null 2>&1 && echo "Done!"
 fi
